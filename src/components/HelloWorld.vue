ae4aade08f2b44542763ad5f9755910448aabdf9
hotMelon <gaoxianglvp@163.com>
2018-01-06 13:25:42 +0800

    finish a flie of counter to test the comunication bettween the parent and child component and get it well done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="hello">
     <h1>{{msg}}</h1>
+    <p>{{pCount}}</p>
+    <v-counter 
+      :count='pCount'
+      @increase='inc' 
+      @decrease='de'></v-counter>
+    
   </div>
 </template>
 
 <script>
+import VCounter from './counter/counter'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'vue 使用的是 模板语法: {{}}  双大括号'
+      msg: 'vue 使用的是 模板语法: {{}}  双大括号',
+      pCount: 1
+    }
+  },
+  components:{
+    VCounter
+  },
+  methods: {
+    inc(){
+      this.pCount ++;
+    },
+    de(){
+      this.pCount--;
     }
   }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='less'>
---
+++ src/components/counter/counter.vue
@@ -0,0 +1,47 @@
+<template>
+ <div class="counter">
+     <button @click="increase">+</button>
+     <span>{{count}}</span>
+     <button @click="decrease">-</button>
+ </div>
+</template>
+
+<script >
+ export default {
+   data() {
+     return {
+     }
+   },
+   props:{
+       count:{
+           type:Number,
+           default: 0
+       }
+   },
+   methods:{
+       increase(){
+           this.$emit('increase');
+       },
+       decrease() {
+        //    this.count --;
+        this.$emit('decrease');
+       }
+   }
+ }
+</script>
+
+<style scoped lang='less'>
+
+    .counter {
+        color:rgb(231, 107, 107);
+        & > * {
+            display: inline-block;
+            vertical-align: top;
+        }
+        & > input {
+            color:red;
+            width: 30px;
+        } 
+    }
+ 
+</style>
